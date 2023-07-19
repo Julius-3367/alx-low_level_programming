195af2911152f9ef5e20210e6cac6772611dc668
Julius-3367 <korirjulius001@gmail.com>
2023-07-19 14:46:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
 
-=======
->>>>>>> 2f62d04534a0c24cdff3cd75a1323deac573f752
 /**
  * main - Entry point
  *
@@ -10,14 +7,8 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
 
-=======
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}
->>>>>>> 2f62d04534a0c24cdff3cd75a1323deac573f752
